2ebdbb36e0f31b66c0ab2f8e5a03ea88b61e25ad
Tek Jones <tekthree@gmail.com>
2021-11-16 12:03:08 -0800

    the css is so slow to respond

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'body': '#17171F',
-      'selected-text': '#A3A3FF',
-      'theme': '#A3A3FF',
-      'nav': '#404053',
-      'secondary': '#9191A4',
-      'badge': '#3F3F51',
-      'input-border': '#565666',
-      'input': '#2A2A35'
+        body: "#17171F",
+        "selected-text": "#A3A3FF",
+        theme: "#A3A3FF",
+        nav: "#404053",
+        secondary: "#9191A4",
+        badge: "#3F3F51",
+        "input-border": "#565666",
+        input: "#2A2A35",
       },
-      fontFamily:{
-        'poppins': ["'Poppins'", 'sans-serif']
-      }
+      fontFamily: {
+        poppins: ["'Poppins'", "sans-serif"],
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
